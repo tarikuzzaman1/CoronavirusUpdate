feb0da569bf2d593249f7203f1e5b658ce63bd34
Tarikuzzaman <tarikuzzaman1@gmail.com>
2020-03-22 18:13:07 +0600

    Initial commit

---
+++ app/src/main/java/com/covid19v2mx/coronavirus/ui/map/MapViewModel.java
@@ -0,0 +1,20 @@
+package com.covid19v2mx.coronavirus.ui.map;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MapViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
